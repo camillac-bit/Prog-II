e9c2cd190dbb9a6b27987e884d3be9ba4c796b18
Camilla Corno <camilla.corno@studenti.unimi.it>
2025-10-05 17:02:28 +0200

    Soluzione 1

--- src/main/java/it/unimi/di/prog2/e03/UguaglianzaFrazioni.java
+++ src/main/java/it/unimi/di/prog2/e03/UguaglianzaFrazioni.java
@@ -31,4 +31,13 @@ public class UguaglianzaFrazioni {
    * corrispondenti rispettivamente a numeratore e denominatore di due frazioni ed emetta nel flusso
    * d'uscita "uguali" se le frazioni sono uguali, oppure "diverse".
    */
+  public static void main(String[] args){
+    int a = Integer.parseInt(args[0]);
+    int b = Integer.parseInt(args[1]);
+    int c = Integer.parseInt(args[2]);
+    int d = Integer.parseInt(args[3]);
+
+    System.out.println(a*d==b*c ? "uguali" : "diverse");
+  }
+  
 }
